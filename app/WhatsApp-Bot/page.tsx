b7024528509100c176d33b44d46a6afbfcbeaaf0
AMAN GUPTA <amanguptatr2002@gmail.com>
2025-10-28 16:39:39 +0530

    Add WhatsApp bot component and integrate into Lots247Page

---
+++ app/WhatsApp-Bot/page.tsx
@@ -0,0 +1,44 @@
+"use client"
+// app/lots-247/page.tsx
+import type React from "react"
+
+import Image from "next/image"
+import { useState, useEffect } from "react"
+
+const whatsAppBot = () => {
+  const [imageSize, setImageSize] = useState(200);
+
+  useEffect(() => {
+    const updateSize = () => {
+      if (window.innerWidth < 640) { // Small mobile screens
+        setImageSize(140);
+      } else if (window.innerWidth < 768) { // Larger mobile screens
+        setImageSize(160);
+      } else if (window.innerWidth < 1024) { // Tablets
+        setImageSize(180);
+      } else if (window.innerWidth < 1280) { // Small laptops
+        setImageSize(190);
+      } else { // Desktops and larger screens
+        setImageSize(200);
+      }
+    };
+
+    updateSize();
+    window.addEventListener('resize', updateSize);
+
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
+  const handleChat = () => {
+    const whatsappUrl = 'https://api.whatsapp.com/send/?phone=919289928628&text=Company+Name%3A+%0ANumber+of+Vehicles%3A+&type=phone_number&app_absent=0';
+    window.open(whatsappUrl, '_blank');
+  };
+
+  return (
+    <div className="fixed bottom-4 right-4 z-50">
+        <Image src="/whatsapp5.png" alt="WhatsApp" width={imageSize} height={imageSize} className="cursor-pointer" onClick={handleChat} />
+    </div>
+  )
+}
+
+export default whatsAppBot
