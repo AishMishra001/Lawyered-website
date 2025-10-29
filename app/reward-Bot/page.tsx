9e8d8d54a5292abb7ac37ae8154960af6c38b093
AMAN GUPTA <amanguptatr2002@gmail.com>
2025-10-29 14:53:47 +0530

    Add RewardBot component and integrate into Home page; update form fields and handle video error state

---
+++ app/reward-Bot/page.tsx
@@ -0,0 +1,43 @@
+"use client"
+// app/reward-Bot/page.tsx
+import type React from "react"
+
+import Image from "next/image"
+import { useState, useEffect } from "react"
+
+const RewardBot = () => {
+  const [imageSize, setImageSize] = useState(200);
+
+  useEffect(() => {
+    const updateSize = () => {
+      if (window.innerWidth < 640) { // Small mobile screens
+        setImageSize(140);
+      } else if (window.innerWidth < 768) { // Larger mobile screens
+        setImageSize(160);
+      } else if (window.innerWidth < 1024) { // Tablets
+        setImageSize(180);
+      } else if (window.innerWidth < 1280) { // Small laptops
+        setImageSize(190);
+      } else { // Desktops and larger screens
+        setImageSize(200);
+      }
+    };
+
+    updateSize();
+    window.addEventListener('resize', updateSize);
+
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
+  const handleRedirect = () => {
+    window.open('https://www.challanpay.in/', '_blank');
+  };
+
+  return (
+    <div className="fixed bottom-4 right-4 z-50">
+        <Image src="/reward.png" alt="Reward" width={imageSize} height={imageSize} className="cursor-pointer" onClick={handleRedirect} />
+    </div>
+  )
+}
+
+export default RewardBot
